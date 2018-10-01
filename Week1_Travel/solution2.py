8c6d12ce3ef54f5ebbde894d09e9fbf3ec03dbbe
suman-d <suman.san14@yahoo.in>
2018-10-01 12:23:37 +0530

    new solution to week1 problem

---
+++ Week1_Travel/solution2.py
@@ -0,0 +1,35 @@
+
+from collections import defaultdict, Counter
+
+visits = defaultdict(Counter)
+
+def collect_places():
+    visits.clear()              # empty out our vists
+    
+    while True:
+
+        location = input("Tell me where you went: ").strip()
+
+        if not location:
+            break
+
+        if location.count(',') != 1:
+            print("That's not a legal city, country combination")
+            continue
+
+        city, country = location.split(',')
+
+        visits[country.strip()][city.strip()] += 1
+
+def display_places():
+    for country, cities in sorted(visits.items()):
+        print(country)
+        for one_city, count in sorted(cities.items()):
+            if count == 1:
+                print(f"\t{one_city}")
+            else:
+                print(f"\t{one_city} ({count})")
+
+if __name__ == '__main__':
+    collect_places()
+    display_places()
